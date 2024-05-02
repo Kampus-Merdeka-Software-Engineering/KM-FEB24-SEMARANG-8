3a9df843254626796d291607a8501baea458e98f
Edo Arya Hermawan <edoarya2002@gmail.com>
2024-05-02 21:28:30 +0800

    ✨ hamburger menu & responsive

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// navbar slide mobile
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
