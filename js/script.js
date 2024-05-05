0f70064093ecf0e26703551ad031b92165bc6c1d
Edo Arya Hermawan <edoarya2002@gmail.com>
2024-05-05 23:09:17 +0800

    ✨ new responsive navbar layout

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// navbar slide mobile
-const menuToggle = document.querySelector(".menu-toggle input");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-});
