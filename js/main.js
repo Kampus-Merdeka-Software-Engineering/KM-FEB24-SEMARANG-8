e3bf4cdfa7b64295f1ddd8c2bfd44544ed20bcca
Edo Arya Hermawan <edoarya2002@gmail.com>
2024-05-06 16:42:42 +0800

    ✨ add scroll header

--- js/main.js
+++ js/main.js
@@ -25,3 +25,11 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// -----change background header-----
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener('scroll', scrollHeader)
