9a81adc4b2ea65d060e7ddfe4ab5821c1c91fafa
Edo Arya Hermawan <edoarya2002@gmail.com>
2024-05-06 11:51:57 +0800

    header & navbar css js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-// navbar slide mobile
-// const menuToggle = document.querySelector(".menu-toggle input");
-// const nav = document.querySelector("nav ul");
+// -----show menu-----
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
-// menuToggle.addEventListener("click", function () {
-//   nav.classList.toggle("slide");
-// });
+// -----menu show-----
+if(navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//-----menu hidden-----
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
