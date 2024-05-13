f9f3efdaaa76f5fcb9cf423b84544b092ef89d5c
edoaryah <edoarya2002@gmail.com>
2024-05-13 16:42:22 +0800

    ✨ landing page update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,56 @@
+// -----show navbar-----
+const navbarMenu = document.getElementById("navbar-menu");
+const navbarOpen = document.getElementById("navbar-open");
+const navbarClose = document.getElementById("navbar-close");
+
 // -----show menu-----
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
-
-// -----menu show-----
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+if (navbarOpen) {
+  navbarOpen.addEventListener("click", () => {
+    navbarMenu.classList.add("show-menu");
   });
 }
 
-//-----menu hidden-----
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+//-----hide menu-----
+if (navbarClose) {
+  navbarClose.addEventListener("click", () => {
+    navbarMenu.classList.remove("show-menu");
   });
 }
 
-// -----remove menu mobile-----
-const navLink = document.querySelectorAll(".nav__link");
+// -----hide menu setelah klik salah satu item
+const navbarLink = document.querySelectorAll(".navbar__link");
 
 function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
+  const navbarMenu = document.getElementById("navbar-menu");
+  navbarMenu.classList.remove("show-menu");
 }
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+navbarLink.forEach((n) => n.addEventListener("click", linkAction));
 
-// -----change background header-----
+// -----show header while scroll >= 50% screen-----
 function scrollHeader() {
   const header = document.getElementById("header");
   if (this.scrollY >= 50) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
+
+// -----swiper js-----
+let swiperPopular = new Swiper(".popular__container", {
+  loop: true,
+  spaceBetween: 24,
+  grabCursor: true,
+
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
