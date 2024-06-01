90fd493e6b6c7cdb643634800e000a022aa37e29
edoaryah <edoarya2002@gmail.com>
2024-06-02 01:20:31 +0800

    add info button

--- js/main.js
+++ js/main.js
@@ -42,54 +42,54 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
-
 // -----------------popup chart Description-----------------
 
-function onClickPopUpc1(){
+function onClickPopUpc1() {
   Swal.fire({
     title: "Revenue Bike And Accecoris Over year",
-    width: 1200,
+    width: '50%',
     html: `
     <p style="font-size: large; text-align: justify;"> Looking at the annual revenue data, it shows that the total accessories revenue is quite far apart from bikes, in addition to more sales and lower prices and accessories sales figures, even in 2015, where the trend was high, revenue from accessories only touched 4,128,261 compared to with 14,742,208 bikes less than a third of the number, this figure shows the disparity between the revenue value between accessories and bikes.<p>
   `,
-    padding: "6em",
+    padding: "2em",
     showClass: {
       popup: `
         animate__animated
         animate__fadeInUp
         animate__faster
-      `
+      `,
     },
     hideClass: {
       popup: `
         animate__animated
         animate__fadeOutDown
         animate__faster
-      `
-    }
+      `,
+    },
   });
 }
-function onClickPopUpc2(){
+
+function onClickPopUpc2() {
   Swal.fire({
     title: "Revenue Bike And Accecoris By Country",
-    width: 1200,
+    width: '50%',
     html: `
     <p style="font-size: large; text-align: justify;"> The data above shows the amount of total revenue from each country, the largest revenue is America with a total revenue of 19,409,654 bikes and accessories of 5,251,088, while the country with the smallest total revenue of bikes is Canada with a total revenue of bikes of 4,262,618. The smallest accessories revenue is France with a figure of 1,337,060. The difference in the amount of revenue is influenced by various reasons, including: trends, services and supplies, behavioral patterns, and etc.<p>
   `,
-    padding: "6em",
+    padding: "2em",
     showClass: {
       popup: `
         animate__animated
         animate__fadeInUp
         animate__faster
-      `
+      `,
     },
     hideClass: {
       popup: `
         animate__animated
         animate__fadeOutDown
         animate__faster
-      `
-    }
+      `,
+    },
   });
 }
