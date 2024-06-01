fe1e2d1fe004504187269ced5d37358947db82a2
edoaryah <edoarya2002@gmail.com>
2024-06-02 01:38:15 +0800

    fix team card bug & add info button each chart

--- js/main.js
+++ js/main.js
@@ -45,9 +45,10 @@ window.addEventListener("scroll", scrollUp);
 // -----------------popup chart Description-----------------
 
 function onClickPopUpc1() {
+
   Swal.fire({
     title: "Revenue Bike And Accecoris Over year",
-    width: '50%',
+    width: 700,
     html: `
     <p style="font-size: large; text-align: justify;"> Looking at the annual revenue data, it shows that the total accessories revenue is quite far apart from bikes, in addition to more sales and lower prices and accessories sales figures, even in 2015, where the trend was high, revenue from accessories only touched 4,128,261 compared to with 14,742,208 bikes less than a third of the number, this figure shows the disparity between the revenue value between accessories and bikes.<p>
   `,
@@ -70,17 +71,116 @@ function onClickPopUpc1() {
 }
 
 function onClickPopUpc2() {
-  // Mendapatkan lebar layar
-  var screenWidth = window.innerWidth;
-
-  // Menentukan lebar popup berdasarkan lebar layar
-  var popupWidth = screenWidth > 767 ? '50%' : 'auto';
 
   Swal.fire({
     title: "Revenue Bike And Accecoris By Country",
-    width: popupWidth,
+    width: 700,
     html: `
     <p style="font-size: large; text-align: justify;"> The data above shows the amount of total revenue from each country, the largest revenue is America with a total revenue of 19,409,654 bikes and accessories of 5,251,088, while the country with the smallest total revenue of bikes is Canada with a total revenue of bikes of 4,262,618. The smallest accessories revenue is France with a figure of 1,337,060. The difference in the amount of revenue is influenced by various reasons, including: trends, services and supplies, behavioral patterns, and etc.<p>
+  `,
+    padding: "2em",
+    showClass: {
+      popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `,
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `,
+    },
+  });
+}
+
+function onClickPopUpc3() {
+
+  Swal.fire({
+    title: "Bike Sales per Subcategory",
+    width: 700,
+    html: `
+    <p style="font-size: large; text-align: justify;"> Sales in the bikes category always show significant figures, namely road bikes have the highest sales, then mountain bikes, and the smallest are touring bikes. The highest sales of road bikes occurred in 2015 with 4,180 units and the lowest in 2013 with 2,756 units. Then the highest sales of mountain bikes occurred in 2015 with 3,114 units and the lowest in 2012 with 1,228 units. Then the highest sales of touring bikes occurred in 2016 with 1,565 units. units and the lowest in 2011 and 2012 with 0 unit sales. The sales volume of bikes is based on the function obtained. Road bikes have models that are popular, especially for mountain bikes for use in the highlands, while touring bikes are more suitable for people to use motorized vehicles.<p>
+  `,
+    padding: "2em",
+    showClass: {
+      popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `,
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `,
+    },
+  });
+}
+
+function onClickPopUpc4() {
+
+  Swal.fire({
+    title: "Bike Sales by Age Group",
+    width: 700,
+    html: `
+    <p style="font-size: large; text-align: justify;"> The chart above shows the amount of bicycle sales based on age group. It is known that the largest sales were obtained by adults (35-64) 48.3% with the number 17,482, then young adults (25-34) 37.7% with the number 13,642, then youth (<25) with a figure of 4,997, and seniors (64+) 0.2% with only 80The average number of bicycle users in the workforce can be based on the fact that it can be reached at a very cheap price, bicycles also reduce traffic jams, save fuel, reduce pollution, and make the body healthy, while seniors experience difficulties in using bicycles and safety when using them.<p>
+  `,
+    padding: "2em",
+    showClass: {
+      popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `,
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `,
+    },
+  });
+}
+
+function onClickPopUpc5() {
+
+  Swal.fire({
+    title: "Bike Sales by Gender",
+    width: 700,
+    html: `
+    <p style="font-size: large; text-align: justify;"> Gender comparison in bicycle sales per year in the chart above shows the largest accumulated comparison of bicycle sales, namely male at 18,262 and female at 15,250 compared to 3,012. or 1/15 on the scale shows that men are more demanding in the use of bicycles and the identification of bicycles with male.<p>
+  `,
+    padding: "2em",
+    showClass: {
+      popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `,
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `,
+    },
+  });
+}
+
+function onClickPopUpc6() {
+
+  Swal.fire({
+    title: "Bike Sales by Country",
+    width: 700,
+    html: `
+    <p style="font-size: large; text-align: justify;"> Bikes sales per country are the same as the total revenue between bikes and accessories in the previous chart where the highest sales are the United States with total sales revenue of 24,661,062 and the lowest is Canada at 6,258,387. In increasing sales levels from the previous 5 years according to the data above So various applications are needed, including providing various new additional models per sub category with analysis according to age and gender in each country, maintaining relationships with customers, implementing cross selling, providing a credible and efficient online sales platform, offering free shipping, reviews and feedback. , attractive promotions, and always developing ways to increase sales by knowing the market and the ability to analyze existing data.<p>
   `,
     padding: "2em",
     showClass: {
--- js/team.js
+++ js/team.js
@@ -1,3 +1,61 @@
+// // Ambil data dari file JSON
+// fetch("data/team.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // Lakukan looping melalui data dan buat kartu untuk setiap anggota tim
+//     data.forEach((member) => {
+//       let teamCard = `
+//                 <article class="team__card swiper-slide">
+//                     <div class="team__image-content">
+//                         <span class="team__image-content-overlay"></span>
+//                         <div class="team__card-image">
+//                             <img src="${member.img}" alt="" class="team__card-img">
+//                         </div>
+//                     </div>
+
+//                     <div class="team__card-content">
+//                         <h2 class="team__card-content-name">${member.name}</h2>
+//                         <h2 class="team__card-content-role">${member.role}</h2>
+//                         <p class="team__card-content-desc">
+//                             ${member.desc}
+//                         </p>
+//                         <p class="team__card-content-prodi">
+//                             ${member.prodi}
+//                         </p>
+//                         <a href="${member.linkedin}" class="team__card-content-button">
+//                             <i class="ri-linkedin-box-fill"></i>
+//                         </a>
+//                     </div>
+//                 </article>
+//             `;
+
+//       // Tambahkan kartu ke dalam DOM
+//       document.querySelector("#swiperWrapper").innerHTML += teamCard;
+//     });
+
+//     // Inisialisasi Swiper setelah slide ditambahkan
+//     let swiperPopular = new Swiper(".team__container", {
+//       loop: true,
+//       spaceBetween: 24,
+//       grabCursor: true,
+
+//       slidesPerView: "auto",
+//       pagination: {
+//         el: ".swiper-pagination",
+//         dynamicBullets: true,
+//       },
+//       breakpoints: {
+//         768: {
+//           slidesPerView: 3,
+//         },
+//         1024: {
+//           spaceBetween: 48,
+//         },
+//       },
+//     });
+//   })
+//   .catch((error) => console.error("Error:", error));
+
 // Ambil data dari file JSON
 fetch("data/team.json")
   .then((response) => response.json())
@@ -33,25 +91,27 @@ fetch("data/team.json")
       document.querySelector("#swiperWrapper").innerHTML += teamCard;
     });
 
-    // Inisialisasi Swiper setelah slide ditambahkan
-    let swiperPopular = new Swiper(".team__container", {
-      loop: true,
-      spaceBetween: 24,
-      grabCursor: true,
+    // Pastikan untuk menunggu semua kartu ditambahkan sebelum menginisialisasi Swiper
+    setTimeout(function() {
+      let swiperPopular = new Swiper(".team__container", {
+        loop: true,
+        spaceBetween: 24,
+        grabCursor: true,
 
-      slidesPerView: "auto",
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 3,
+        slidesPerView: "auto",
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
         },
-        1024: {
-          spaceBetween: 48,
+        breakpoints: {
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            spaceBetween: 48,
+          },
         },
-      },
-    });
+      });
+    }, 0);
   })
   .catch((error) => console.error("Error:", error));
