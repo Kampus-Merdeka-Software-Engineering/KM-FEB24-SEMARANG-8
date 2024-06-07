69aa447a28cab5ce118f83d497165a86cae98a57
YusufNM1107 <runruntemboro123@gmail.com>
2024-06-08 03:20:51 +0700

    font datatables,adding contact us form,validate js,send email with emailjs(error)

--- js/main.js
+++ js/main.js
@@ -267,5 +267,40 @@ anime.timeline({loop: true})
         clearInterval(loaded)
         loading.style.display = 'none'; 
         document.getElementById('main-content').style.display = 'block';
-    }, 4000)
+    }, 3000)
+}
+
+
+// Validate And Send Form
+
+(function() {
+  emailjs.init("QZbUBCAwKc93d-bG7Aglv");
+})();
+
+function sendEmail() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  if (name === "" || email === "" || message === "") {
+      alert("Semua field harus diisi!");
+      return false;
+  }
+
+  var templateParams = {
+      from_name: name,
+      from_email: email,
+      message_html: message
+  };
+
+  emailjs.send('service_fh14nhs', templateParams)
+      .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Email berhasil dikirim!');
+      }, function(error) {
+          console.log('FAILED...', error);
+          alert('Gagal mengirim email!');
+      });
+
+  return false; // Untuk mencegah submit form
 }
